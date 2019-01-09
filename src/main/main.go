963835b2a5c62049e59dd4e953e86a767d889d16
Popurey Sergey <pprisn@yandex.ru>
2019-01-09 19:40:02 +0300

    p

--- src/main/main.go
+++ src/main/main.go
@@ -286,7 +286,7 @@ func SendMessageToBot(botID string, rtext string) {
 	//values := url.Values{}
 	//values.Add("access_token", AccessToken)
 	//req.URL.RawQuery = values.Encode()
-	req.Header.Add(`Content-Type`, `application/json; charset=UTF-8`)
+	req.Header.Add(`"Content-Type"`, `"application/json; charset=UTF-8"`)
 	client := &http.Client{Timeout: time.Duration(30 * time.Second)}
 
 	log.Printf("req=%+v\n", req)
