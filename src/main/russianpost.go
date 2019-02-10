68f6d63ee8123831277d5b63ec9ef1a9fa772fa4
Popurey Sergey <pprisn@yandex.ru>
2019-02-10 22:35:53 +0300

    link pdf

--- src/main/russianpost.go
+++ src/main/russianpost.go
@@ -221,8 +221,8 @@ func req2russianpost(barcode string) string {
 	for i, rec := range envelope.Body.GetOperationHistoryResponse.OperationHistoryData.HistoryRecord {
 		if i == 0 {
 
-                        Delivstatus = append(Delivstatus, fmt.Sprintf("Отправитель %v \t",rec.UserParameters.sndr))
-                        Delivstatus = append(Delivstatus, fmt.Sprintf("Получатель %v \t",rec.UserParameters.rcpn))
+                        Delivstatus = append(Delivstatus, fmt.Sprintf("Отправитель %v \t",rec.UserParameters.Sndr))
+                        Delivstatus = append(Delivstatus, fmt.Sprintf("Получатель %v \t",rec.UserParameters.Rcpn))
 			Delivstatus = append(Delivstatus, fmt.Sprintf("%v Масса=%vгр.", rec.ItemParameters.ComplexItemName, rec.ItemParameters.Mass))
 			Delivstatus = append(Delivstatus, fmt.Sprintf("%v %v\t %v\t", string(rec.OperationParameters.OperDate)[:19],
 				rec.AddressParameters.OperationAddress.Description,
